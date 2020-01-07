77429d51a4edac37d9a1f20d4eb9eefa0683eea2
Rody Davis <rody@rive.app>
2020-01-07 15:14:22 -0800

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'package:window_utils/window_utils.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => WindowUtils.hideTitleBar(),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: GestureDetector(
+                  onTapDown: (_) {
+                    WindowUtils.startDrag();
+                  },
+                  child: Material(
+                    elevation: 4.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: IgnorePointer(
+                  child: Center(
+                    child: Text(
+                      'Window Utils Example',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                top: 0,
+                right: 0,
+                child: Center(
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        color: Colors.white,
+                        icon: Icon(Icons.info_outline),
+                        onPressed: () {
+                          WindowUtils.getWindowSize().then(print);
+                          WindowUtils.getWindowOffset().then(print);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
